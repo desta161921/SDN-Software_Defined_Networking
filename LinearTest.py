7dd8a9539376ff0232816cda1cc6cd36d6821cc9
Desta <desta161921@gmail.com>
2014-06-11 12:21:23 -0700

    Linear Topology test

---
+++ LinearTest.py
@@ -0,0 +1,12 @@
+#__author='desta'
+#/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.topo import LinearTopo
+
+Linear=LinearTopo(k=4)
+net=Mininet(topo=Linear)
+
+net.start()
+net.pingAll()
+net.stop()
